23a64ab59fcfa3bf511799d954892fddee0f5e95
elidallen <mitchellallen2@yahoo.com>
2023-10-20 13:36:21 -0400

    update

--- index.js
+++ index.js
@@ -7,37 +7,42 @@ function startGame() {
     const randomNumber = Math.floor(Math.random() * 100) + 1;
     let attempts = 0;
 
-    while (attempts < maxAttempts) {
+    // Function to handle user guess
+    function askForGuess() {
         const userGuess = prompt("Guess the number (between 1 and 100):");
 
         if (userGuess === null) {
-            // User clicked Cancel or closed the prompt
             alert("Game over. You quit!");
-            break;
-        }
-
-        const guess = parseInt(userGuess);
-
-        if (isNaN(guess) || guess < 1 || guess > 100) {
-            alert("Please enter a valid number between 1 and 100.");
         } else {
-            attempts++;
+            const guess = parseInt(userGuess);
 
-            if (guess === randomNumber) {
-                alert(`Congratulations! You guessed the correct number (${randomNumber}) in ${attempts} attempts.`);
-                break;
-            } else if (guess < randomNumber) {
-                alert("Try a higher number.");
+            if (isNaN(guess) || guess < 1 || guess > 100) {
+                alert("Please enter a valid number between 1 and 100.");
             } else {
-                alert("Try a lower number.");
+                attempts++;
+
+                if (guess === randomNumber) {
+                    alert(`Congratulations! You guessed the correct number (${randomNumber}) in ${attempts} attempts.`);
+                } else if (guess < randomNumber) {
+                    alert("Try a higher number.");
+                } else {
+                    alert("Try a lower number.");
+                }
+
+                if (attempts < maxAttempts) {
+                    askForGuess();
+                } else {
+                    alert(`You've reached the maximum number of attempts. The correct number was ${randomNumber}.`);
+                }
             }
         }
     }
 
-    if (attempts >= maxAttempts) {
-        alert(`You've reached the maximum number of attempts. The correct number was ${randomNumber}.`);
-    }
+    askForGuess(); // Start the guessing process
 }
 
 // Event listener for the start button
-startButton.addEventListener("click", startGame);
+startButton.addEventListener("click", function() {
+    console.log("Button clicked!");
+    startGame(); // Call your game function here
+});
